0cc4521a7a2ab67e704e1b3d3e4af73b7a72dc12
Andrew Steinborn <git@steinborn.me>
2024-01-21 19:13:13 -0500

    🔥 optimized blitting

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,36 @@ use sdl2::video::Window;
 use std::time::Duration;
 use std::time::Instant;
 
+static SIXTY_FPS_BUDGET: Duration = Duration::new(0, 1_000_000_000u32 / 60);
+
 fn draw_board(board: &ConwayBoard, canvas: &mut Canvas<Window>, scalar: usize) {
+    let scalar_u32 = scalar as u32;
+
+    let mut live_cells = Vec::new();
+    let mut dead_cells = Vec::new();
+
     for x in 0..board.width {
         for y in 0..board.height {
-            let color = match board.get(x, y) {
-                true => Color::RGB(255, 255, 255),
-                false => Color::RGB(0, 0, 0)
-            };
-            canvas.set_draw_color(color);
-            canvas.fill_rect(
-                Rect::new((x * scalar) as i32, (y * scalar) as i32, scalar as u32, scalar as u32)
-            ).unwrap();
+            let cell_rect = Rect::new(
+                (x * scalar) as i32,
+                (y * scalar) as i32,
+                scalar_u32,
+                scalar_u32
+            );
+
+            if board.get(x, y) {
+                live_cells.push(cell_rect);
+            } else {
+                dead_cells.push(cell_rect);
+            }
         }
     }
+
+    canvas.set_draw_color(Color::RGB(0, 128, 0));
+    canvas.fill_rects(&live_cells[..]).unwrap();
+
+    canvas.set_draw_color(Color::RGB(128, 0, 0));
+    canvas.fill_rects(&&dead_cells[..]).unwrap();
 
     // draw a grid
     if scalar > 1 {
@@ -50,9 +67,9 @@ pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let board_width = 800;
-    let board_height = 600;
-    let scalar = 1;
+    let board_width = 200;
+    let board_height = 100;
+    let scalar = 6;
 
     let mut board = ConwayBoard::new_drylife(board_width, board_height);
     board.randomize();
@@ -68,9 +85,14 @@ pub fn main() {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
+
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut rounds = 0;
+    let mut frames = 0;
     let mut paused = true;
+
+    let mut prev_times = vec![Instant::now(); 60];
+
     'running: loop {
         let now = Instant::now();
 
@@ -103,20 +125,26 @@ pub fn main() {
         // The rest of the game loop goes here...
 
         if !paused {
-            let sim_start = now.elapsed();
             board.simulate();
-            let sim_end = now.elapsed();
-            println!("Simulation took {:#?}", sim_end - sim_start);
             rounds += 1;
         }
 
-        canvas.window_mut().set_title(format!("game of life (rounds: {}, paused: {})", rounds, paused).as_str());
-
         canvas.present();
 
-        let elapsed = now.elapsed();
-        println!("Simulation and drawing took {:#?}", elapsed);
+        let ideal_fps = prev_times.len();
+        let now2 = Instant::now();
+        let elapsed = now2 - now;
+        let fps = ideal_fps as f64 / (now2 - prev_times[frames % ideal_fps]).as_secs_f64();
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        canvas.window_mut().set_title(format!("game of life (rounds: {}, paused: {}, fps: {:.2})", rounds, paused, fps).as_str());
+        prev_times[frames % ideal_fps] = now2;
+        frames += 1;
+
+        println!("{}", fps);
+
+        if elapsed < SIXTY_FPS_BUDGET {
+            // We have some time to spare, go to sleep
+            ::std::thread::sleep(SIXTY_FPS_BUDGET - elapsed);
+        }
     }
 }
